bab75f66cf44ff6c76415d0ca171187ec30f35ca
COWELL\yiming.hsu <yiming.hsu@cowell.com.tw>
2017-09-01 18:43:07 +0800

    no message

--- public/content/themes/twentytwelve2/header.php
+++ public/content/themes/twentytwelve2/header.php
@@ -29,7 +29,7 @@
 <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 <![endif]-->
-<script src="<?php echo get_template_directory_uri(); ?>/js/syntaxhighlighter.js" type="text/javascript"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/syntaxhighlighter.js" type="text/javascript" class="brush: js"></script>
 <?php wp_head(); ?>
 </head>
 
