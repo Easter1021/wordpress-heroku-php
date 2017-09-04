7acf7267c1017cab1fad27358a3cb9249e9aaa2e
easter1021 <mufasa.hsu@gmail.com>
2017-09-04 14:56:36 +0800

    FB something

--- public/content/themes/twentytwelve2/header.php
+++ public/content/themes/twentytwelve2/header.php
@@ -40,6 +40,14 @@
 </head>
 
 <body <?php body_class(); ?>>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/zh_TW/sdk.js#xfbml=1&version=v2.10&appId=1071737769629006";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
 <div id="page" class="hfeed site">
 	<header id="masthead" class="site-header" role="banner">
 		<hgroup>
